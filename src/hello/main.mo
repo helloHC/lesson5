9b388ded55f427bf6d000897aa267b8af93d9e5b
helloHC <86517641@qq.com>
2022-03-22 01:53:33 +0800

    first commit

--- src/hello/main.mo
+++ src/hello/main.mo
@@ -1,5 +1,68 @@
-actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+import Iter "mo:base/Iter";
+import List "mo:base/List";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+
+actor Microblog {
+    public type Message = {
+        content: Text;
+        sinced: Time.Time;
+        author: Text;
+    };
+
+    public type Microblog = actor {
+        follow: shared(Principal) -> async ();
+        follows: shared query () -> async [Principal];
+        post: shared (Text, Text) -> async ();
+        posts: shared query (Time.Time) -> async [Message];
+        timeline: shared (Time.Time) -> async [Message];
+    };
+
+    stable var followed: List.List<Principal> = List.nil();
+    stable var name: Text = "";
+
+    public shared func follow(id: Principal) : async () {
+        followed := List.push(id, followed)
+    };
+
+    public shared query func follows(): async [Principal] {
+        List.toArray(followed)
+    };
+
+    stable var messages: List.List<Message> = List.nil();
+
+    public shared func post(otp: Text, content: Text): async () {
+        // assert(Principal.toText(msg.caller) == "l6z5v-7lcmi-tzj4u-362dn-2tvfs-7slx3-hlc6m-k7hsn-6nno2-gtdiw-rqe");
+        assert(otp == "111");
+        let sinced = Time.now();
+        let author = Principal.toText(Principal.fromActor(Microblog));
+        messages := List.push({ content; sinced; author }, messages);
+    };
+
+    public shared query func posts(since: Time.Time): async [Message] {
+        List.toArray(List.filter<Message>(messages, func ({sinced}) = sinced >= since))
+    };
+
+    public shared func timeline(since: Time.Time): async [Message] {
+        var all : List.List<Message> = List.nil();
+
+        for(id in Iter.fromList(followed)) {
+            let canister : Microblog = actor(Principal.toText(id));
+            let msgs = await canister.posts(since);
+            for (msg in Iter.fromArray(msgs)) {
+                all := List.push(msg, all);
+            };
+        };
+
+        List.toArray(all)
+    };
+
+    public shared query func get_name(): async ?Text {
+        ?name
+    };
+
+    public shared func set_name(otp: Text, newName: Text): async () {
+        assert(otp == "111");
+        name := newName;
     };
 };
--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -1,19 +1,146 @@
 import { hello } from "../../declarations/hello";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const padding = (s, len, isZero) => {
+  if (isZero) {
+    return s;
+  } else {
+    len = len - (s + '').length;
+    for (let i = 0; i < len; i++) {
+      s = '0' + s;
+    }
+    return s;
+  }
+}
 
-  const name = document.getElementById("name").value.toString();
+const formatData = (date, pattern, isZero) => {
+  let newDate;
+  switch (typeof (date)) {
+    case 'string':
+      newDate = new Date(date);
+      break; //字符串格式
+    case 'number':
+      newDate = new Date();
+      newDate.setTime(date * 1000);
+      break; //时间戳格式
+    case 'object':
+      newDate = date;
+      break;
+    default:
+      return date;
+  }
+  if (!newDate) {
+    return '-';
+  } else {
+    pattern = pattern || 'yyyy-MM-dd';
+    return pattern.replace(/([yMdhsmw])(\1*)/g, function ($0) {
+      switch ($0.charAt(0)) {
+        case 'y':
+          return padding(newDate.getFullYear(), $0.length, isZero);
+        case 'M':
+          return padding(newDate.getMonth() + 1, $0.length, isZero);
+        case 'd':
+          return padding(newDate.getDate(), $0.length, isZero);
+        case 'w':
+          return newDate.getDay();
+        case 'h':
+          return padding(newDate.getHours(), $0.length, isZero);
+        case 'm':
+          return padding(newDate.getMinutes(), $0.length, isZero);
+        case 's':
+          return padding(newDate.getSeconds(), $0.length, isZero);
+      }
+    });
+  }
+}
 
-  button.setAttribute("disabled", true);
+// async function post() {
+//   const post_button = document.getElementById('post');
+//   const error = document.getElementById("error");
+//   error.innerText = "";
+//   post_button.disabled = true;
+//   const textarea = document.getElementById("message");
+//   const otp = document.getElementById("otp").value;
+//   const text = textarea.value;
+//   try {
+//     await hello.post(otp, text);
+//     textarea.value = "";
+//     load_posts()
+//   } catch (err) {
+//     console.log(err);
+//     error.innerText = "发布失败";
+//   }
+//   post_button.disabled = false;
+// }
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello.greet(name);
+// let num_posts = 0;
+const getPostsList = async () => {
+  const posts = await hello.timeline(new Date().getTime());
+  return posts;
+}
 
-  button.removeAttribute("disabled");
+const renderPostsList = _posts => {
+  const posts_section = document.querySelector("#posts");
+  const fragement = document.createDocumentFragment();
+  posts_section.replaceChildren([]);
+  _posts.forEach(item => {
+    const post = document.createElement("p");
+    const time = formatData(parseInt(item.sinced.toString().substring(0, 10)));
+    post.innerText = `${item.content}---${time}---${item.author}`;
+    fragement.appendChild(post);
+  })
+  posts_section.appendChild(fragement);
+}
 
-  document.getElementById("greeting").innerText = greeting;
+// async function load_posts() {
+//   const posts_section = document.getElementById("posts");
+//   let posts = await hello.posts(new Date().getTime());
 
-  return false;
-});
+//   if (num_posts == posts.length) return;
+//   posts_section.replaceChildren([]);
+
+//   const fragement = document.createDocumentFragment();
+
+//   for (let i = 0; i < posts.length; i++) {
+//     const post = document.createElement("p");
+//     post.innerText = `${posts[i].content}---${posts[i].sinced}---${posts[i].author}`;
+//     fragement.appendChild(post);
+//   }
+//   posts_section.appendChild(fragement);
+// }
+
+const getFollowList = async () => {
+  const res = await hello.follows();
+  const fragement = document.createDocumentFragment();
+  res.forEach(principal => {
+    const follower = document.createElement("p");
+    follower.innerText = principal.toString();
+    follower.setAttribute('principal', principal.toString())
+    fragement.appendChild(follower);
+  })
+  document.querySelector(".follow-list").appendChild(fragement)
+}
+
+function load() {
+  // const post_button = document.getElementById("message");
+  // post_button.onclick = post;
+  // load_posts();
+  // setInterval(load_posts, 3000);
+  let postsList;
+  getPostsList().then(res => {
+    postsList = res;
+    renderPostsList(res)
+  })
+
+  getFollowList();
+
+  document.querySelector(".follow-list").addEventListener('click', function (e) {
+    const _this = e.target;
+    postsList && postsList.filter(item => item.author === _this.getAttribute('principal')).forEach(item => renderPostsList([item]))
+  })
+
+  document.querySelector(".reset-btn").addEventListener('click', function (e) {
+    postsList && renderPostsList(postsList)
+  })
+}
+
+window.onload = load
